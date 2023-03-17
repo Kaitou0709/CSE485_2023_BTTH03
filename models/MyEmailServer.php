49cedded01f6d8e296609d42d2933061e138379a
Kaitou0709 <99159633+Kaitou0709@users.noreply.github.com>
2023-03-17 09:50:26 +0700

    sua pass

--- models/MyEmailServer.php
+++ models/MyEmailServer.php
@@ -14,7 +14,7 @@ class MyEmailServer implements EmailServerInterface
             $mail->Host = 'smtp.gmail.com'; //Set the SMTP server to send through
             $mail->SMTPAuth = true; //Enable SMTP authentication
             $mail->Username = 'tuanbq12@gmail.com'; //SMTP username
-            $mail->Password = 'tskdefuoykuofgif'; //SMTP password
+            $mail->Password = 'jhxavzvsexedfydp'; //SMTP password
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; //Enable implicit TLS encryption
             $mail->Port = 465; //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
             $mail->CharSet = 'UTF-8';
